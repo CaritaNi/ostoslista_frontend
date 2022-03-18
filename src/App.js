a9fc1bb0fbc16d708a7619922766f11b754101af
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2022-03-18 21:26:26 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost/ostoslista/ostoslista.php/';
+
 
 function App() {
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState([]);
+  const [amount, setAmount] = useState([]);
+
+  useEffect(() =>{
+    axios.get(URL)
+      .then((response) =>{
+        setItems(response.data);
+      }).catch(error => {
+          alert(error.response ? error.response.data.error : error);
+        });
+  }, [])
+
+
+function save(e) {
+  e.preventDefault();
+  const json = JSON.stringify({description:items,amount:amount});
+      console.log(json);
+  axios.post(URL + 'add.php',json, {
+    headers: {
+      'Content-type' : 'application/json'
+    }
+  })
+  .then((response) => {
+     setItems(items => [...items,response.data]);
+     setItem('');
+     setAmount('');
+  }).catch(error => {
+     alert(error.response ? error.response.data.error : error);
+  })
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={save}>
+        <h1> Ostoslista</h1> <br></br>
+        <input value={item} placeholder='Lisää tuote' type='text' onChange={e => setItem(e.target.value)} /> 
+        <input value={amount} placeholder= 'Lisää määrä' onChange={e => setAmount(e.target.value)} /> 
+        <button> Lisää </button>
+      </form>
+      <ul>
+          {items?.map(item => (
+            <li key={item.id}>{item.description}</li>
+          ))}
+      </ul>
     </div>
-  );
+    );
 }
 
 export default App;
